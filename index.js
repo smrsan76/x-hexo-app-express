11231e558dbd991625e697bc0c510788c5abf805
smrsan76 <mreza.ndf@gmail.com>
2018-03-09 15:29:42 +0330

    x-hexo-app-connect new codes applied on x-hexo-app-express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* global hexo */
 "use strict";
 
-exports = module.exports = function(app, hexoInstance) {
+exports = module.exports = function(app, hexoInstance, opts){
 
     /*/ Dependencies /*/
     var express = require('express');
@@ -11,81 +11,92 @@ exports = module.exports = function(app, hexoInstance) {
     /*/ Requirements /*/
     var hexoApp = express();
 
-    function run() {
+    function run(){
 
-        return new Promise(function (resolve, reject) {
+        return new Promise(function(resolve, reject){
 
+            /*/ Step 1 => Get Configs/Options from The Third Argument /*/
+            opts = typeof(opts) !== "object" ? {} : opts;
 
-            /*/ Step 1 => Initialize Hexo /*/
+            var config = assign({
+                log: false,
+                compress: false,
+                header: true,
+                serveStatic: false,
+                route: '/'
+            }, opts);
+
+            /*/ Step 2 => Initialize Hexo /*/
             hexoInstance
                 .init()
-                .then(function () {
+                .then(function(){
 
-                    /*/ Step 2 => Get Hexo Configs /*/
-                    var config = hexoInstance.config.app;
-                    config = assign({
-                        log: false,
-                        compress: false,
-                        header: true,
-                        serveStatic: false,
-                        route: '/'
-                    }, config);
+                    /*/ Step 3 => Get Hexo Configs from _config.yml File /*/
+                    config = typeof(hexoInstance.config.app) !== "object" ?
+                                config:
+                                assign(
+                                    assign({}, config),
+                                    hexoInstance.config.app
+                                );
 
-                    /*/ Step 3 => Overwrite url_for() helper /*/
+                    /*/ Step 4 => Apply Configs/Options on The Configs of Intended Hexo Instance /*/
+                    hexoInstance.config.app = config;
+
+                    /*/ Step 5 => Overwrite url_for() helper /*/
                     hexoInstance.extend.helper.register('url_for', require('./lib/helper/url_for'));
 
-                    /*/ Step 4 => Extend Hexo with App /*/
+                    /*/ Step 6 => Extend Hexo with App /*/
                     hexoInstance
                         .extend
                         .filter
                         .exec('server_middleware', hexoApp, {context: hexoInstance})
-                        .catch(function (err) {
+                        .catch(function(err){
 
                             hexoInstance.log.fatal('An error has occurred while extending hexo with app.');
 
                             reject(err);
 
-                            try {
+                            try{
                                 hexoInstance.unwatch();
-                            } catch (err) {
-                            }
+                            } catch(err) {}
 
                             hexoInstance.exit();
 
                         })
-                        .then(function () {
+                        .then(function(){
 
-                            /*/ Step 5 => Run (Watch/Load) Hexo /*/
-                            if (config.serveStatic)
+                            /*/ Step 7 => Run (Watch/Load) Hexo /*/
+                            if(config.serveStatic)
 
                                 return hexoInstance.load();
 
                             return hexoInstance.watch();
 
                         })
-                        .then(function () {
+                        .then(function(){
 
                             hexoInstance.log.info('[Hexo-App-Express] is running on route ' + config.route);
-                            resolve(config);
+
+                            /*/ Step 8 => Use hexoApp as route in your app /*/
+                            app.use(config.route, hexoApp);
 
                         })
-                        .catch(function (err) {
+                        .catch(function(err){
 
                             hexoInstance.log.fatal('An error has occurred while running hexo.');
 
                             reject(err);
 
-                            try {
+                            try{
                                 hexoInstance.unwatch();
-                            } catch (err) {
-                            }
+                            } catch(err) {}
 
                             hexoInstance.exit();
 
                         });
 
                 })
-                .catch(function (err) {
+                .catch(function(err){
 
                     hexoInstance.log.fatal('An error has occurred while initializing hexo.');
                     hexoInstance.exit();
@@ -98,7 +109,6 @@ exports = module.exports = function(app, hexoInstance) {
 
     }
 
-
     hexoInstance.extend.filter.register('server_middleware', require('./lib/middleware/header'));
     hexoInstance.extend.filter.register('server_middleware', require('./lib/middleware/gzip'));
     hexoInstance.extend.filter.register('server_middleware', require('./lib/middleware/logger'));
@@ -106,14 +116,7 @@ exports = module.exports = function(app, hexoInstance) {
     hexoInstance.extend.filter.register('server_middleware', require('./lib/middleware/static'));
     hexoInstance.extend.filter.register('server_middleware', require('./lib/middleware/redirect'));
 
-    run()
-        .then(function(appConfig){
-
-            /*/ Step 6 => Use hexoApp as route in your app /*/
-            app.use(appConfig.route, hexoApp);
-
-        });
-
+    run();
 
     return hexoApp;
 
